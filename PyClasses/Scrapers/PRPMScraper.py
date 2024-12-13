71ed3fa9d1d76651c86427f92a77074844d332cf
YunnNing <yunningtoh@gmail.com>
2024-12-13 12:16:42 +0800

    Changes to be committed: 	new file:   PyClasses/Scrapers/PRPMCleaner.py 	modified:   PyClasses/Scrapers/PRPMScraper.py 	modified:   Webscraping.ipynb

--- PyClasses/Scrapers/PRPMScraper.py
+++ PyClasses/Scrapers/PRPMScraper.py
@@ -13,105 +13,60 @@ class PRPMScraper:
     def findWordMetaData(self, word):
         urlReq = self.baseUrl + word
         responsedHTML = requests.get(urlReq)
-        soup = BeautifulSoup(responsedHTML.content,'html.parser')
+        soup = BeautifulSoup(responsedHTML.content, 'html.parser')
         self.word = word
         self.meanings = self.getWordMeanings(soup)
         self.synonym = self.getSynonym(soup)
         if self.meanings:
             return {self.word: {"meanings": self.meanings, "synonym": self.synonym}}
         return None
-    
+
     def getWordMeanings(self, soup):
         meanings = []
-        # Extract all definitions
         tabs = soup.find_all('div', class_="tab-pane")
-        
-        for tab in tabs:
-            # Step 1: Remove "Definisi :" at the start
-            cleaned_text = re.sub(r".*?Definisi\s*:\s", "", tab.get_text())
 
-            # Step 2: Remove the last parenthetical block `(Kamus Inggeris-Melayu Dewan)`
-            cleaned_text = re.sub(r"\s*\(K.*?\)$", "", cleaned_text)
-            meanings.append(cleaned_text)
-        return(meanings)
+        for tab in tabs:
+            text = tab.get_text()
+            index = 1  # Start with detecting 1
+
+            segments = re.split(r'\b(\d+)\b\.\s*', text)
+            current_number = None
+
+            findOne = re.search(r'\b1\b', text)
+            start_index = text.find("Definisi :")
+            
+            if not findOne:
+                if start_index != -1: 
+                    definition = text[start_index + len("Definisi :"):].strip()
+                    first_meaning = re.split(r'[;:]', definition)[0].strip()
+                    meanings.append(first_meaning)
+                    break
+           
+            if findOne:    
+                for i in range(1, len(segments), 2):
+                    number = int(segments[i])  
+                    content = segments[i + 1] 
     
-    def getSynonym(self, soup):
-        synonymList = []
-        contents = soup.find_all('b', string='Bersinonim dengan ')
-        for content in contents:
-            synonym = content.find_next_sibling('a').text
-            synonymList.append(synonym)
-        return synonymList
+                    if current_number is None:
+                        current_number = number
+    
+                    if number == 1 and current_number != 1:
+                        break
+    
+                    if number == index:
+                        meaning = re.split(r'[:;]', content)[0].strip()
+                        meanings.append(meaning)
+                        index += 1  
+                    else:
+                        break  
+        return meanings
+
 
     def getSynonym(self, soup):
         synonymList = []
         contents = soup.find_all('b', string='Bersinonim dengan ')
         for content in contents:
             all_synonyms = content.find_next_siblings('a')
-            for synonym in all_synonyms: 
+            for synonym in all_synonyms:
                 synonymList.append(synonym.text.strip())
         return synonymList
-
-    
-    # def resultExits(soup):
-    #     content = soup.find('table', class_='info')
-    #     result = content.find('b').text
-    #     print(result)
-    #     if('Tiada maklumat' in result):
-    #         return False
-    #     return True
-
-    # def getPRPMSearchResult(record):
-    #     return dict([self.word, dict(meanings = self.meanings,synonym=self.synonym)])
-
-
-# import requests
-# from bs4 import BeautifulSoup
-
-
-# ##PRPM web scraping
-# class PRPMScraper:
-#     def __init__(self):
-#         self.baseUrl = 'https://prpm.dbp.gov.my/Cari1?keyword='
-#         self.word = None
-#         self.meanings = None
-#         self.sinonim = None
-
-#     def findWordMetaData(self, word):
-#         urlReq = self.baseUrl + word
-#         responsedHTML = requests.get(urlReq)
-#         soup = BeautifulSoup(responsedHTML.content,'html.parser')
-#         self.word = word
-#         self.meanings = self.getWordMeanings(soup)
-#         self.sinonim = self.getSinonim(soup)
-#         if self.meanings:
-#             return {self.word: {"meanings": self.meanings, "sinonim": self.sinonim}}
-#         return None
-    
-#     def getWordMeanings(self, soup):
-#         meanings = []
-#         contents = soup.find_all('div', class_='tab-pane')
-#         for content in contents:
-#             defiTab = content.find('b', string='Definisi : ')
-#             meanings.append(defiTab.next_sibling.strip())
-#         return(meanings)
-    
-#     def getSinonim(self, soup):
-#         sinonimList = []
-#         contents = soup.find_all('b', string='Bersinonim dengan ')
-#         print(contents)
-#         for content in contents:
-#             sinonim = content.find_next_sibling('a').text
-#             sinonimList.append(sinonim)
-#         return sinonimList
-    
-#     # def resultExits(soup):
-#     #     content = soup.find('table', class_='info')
-#     #     result = content.find('b').text
-#     #     print(result)
-#     #     if('Tiada maklumat' in result):
-#     #         return False
-#     #     return True
-
-#     # def getPRPMSearchResult(record):
-#     #     return dict([self.word, dict(meanings = self.meanings,sinonim=self.sinonim)])
