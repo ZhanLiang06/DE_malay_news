0822e14ce748c80f9f97a98100da35c07477fa05
ZhanLiang06 <141556142+ZhanLiang06@users.noreply.github.com>
2024-12-22 23:32:00 +0800

    Add files via upload

---
+++ lexicraft/util/redis/freq_queries.py
@@ -0,0 +1,11 @@
+# Create a class that will be used by clients so that they can access the db and retrieve any words they want
+# Implement Redis
+# Set up Redis
+# Will keep frequently queried queries --> will check if present, if no then will store again
+
+from neo4j import GraphDatabase
+import redis
+
+class Neo4j_Redis_Client:
+    pass
+    
